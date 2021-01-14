f95c527fe42b12be46755a558dafe71f334a0d3a
Edvin13 <Edvin10@bk.ru>
2021-01-15 00:13:12 +0400

    ..

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -5,4 +5,4 @@ from .models import News
 
 def index(request):
      news = News.objects.all()
-     return render(request,'news/index.html',{'news':news}) 
+     return render(request,'news/index.html',{'news':news,'title': 'Список новостей' }) 
