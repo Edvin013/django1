a4e6795d1c05f4726cbe642427f390136b373c5d
Edvin <Edvin10@bk.ru>
2021-01-24 21:20:09 +0400

    24.01.2021 подключение фоток , Создание ссылки на категории , новый URL

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-path('',index),
+    path('', index, name='home'),
+    path('Category/<int:Category_id>/', get_category, name='Category'),
 
 ]
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 
 def index(request):
-     news = News.objects.all()
-     return render(request,'news/index.html',{'news':news,'title': 'Список новостей' }) 
+    news = News.objects.all()
+    Categories = Category.objects.all()
+
+    context = {
+        'news': news,
+        'title': 'Список новостей',
+        'Categories': Categories
+    }
+    return render(request,  template_name='news/index.html', context=context)
+
+
+def get_category(request, Category_id):
+    news = News.objects.filter(Category_id=Category_id)
+    Categories = Category.objects.all()
+    category = Category.objects.get(pk=Category_id)
+    return render(request, 'news/Category.html', {'news': news, 'Categories': Categories, 'category': category})
